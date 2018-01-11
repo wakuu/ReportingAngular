735cbd6a858dc5ddf4900bcc86b13e17a3cfe5f4
wakuu <lucas.guerrot@hotmail.fr>
2018-01-11 14:54:24 +0100

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { ReportingComponent } from './components/reporting/reporting.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ReportingCaComponent } from './components/reporting/reporting-ca/reporting-ca.component';
+import { DataTableModule } from 'primeng/primeng';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReportingComponent
+    ReportingComponent,
+    ReportingCaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DataTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
